e07ccef182ffe14d9d9e5afbaca4d3b4bca6c835
LuisPulenta <luisalbertonu@gmail.com>
2024-03-27 10:51:08 -0300

    Agregar archivos de proyecto.

---
+++ ApiBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiBlog.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBlog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //Agregar modelos
+        public DbSet<Post> Posts { get; set; }
+     }
+}
---
+++ ApiBlog/Modelos/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiBlog.Modelos
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; } = null!;
+        [Required]
+        public string Descripcion { get; set; } = null!;
+        public string? RutaImagen { get; set; }
+        [Required]
+        public string Etiquetas { get; set; } = null!;
+        public DateTime FechaCreacion { get; set; }=DateTime.Now;
+        public DateTime FechaActualizacion { get; set; }
+    }
+}
